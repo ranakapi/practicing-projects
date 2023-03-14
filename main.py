1764f8a1ce57e948ebed4830c2e38cae782a90a7
ranakapi <45564156+ranakapi@users.noreply.github.com>
2023-03-14 09:33:01 +0000

    Adding Banner Pictures

--- main.py
+++ main.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
   return render_template('home.html')
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+
+@app.route("/page1.html")
+def gents():
+  return render_template('page1.html')
+
+
+@app.route("/page2.html")
+def ladies():
+  return render_template('page2.html')
+
+
+@app.route("/page3.html")
+def kids():
+  return render_template('page3.html')
+
+
+@app.route("/contact.html")
+def contact():
+  return render_template('contact.html')
+
+
+app.run(host='0.0.0.0', debug=True)
